133b9aa0363d416e9f55472007d01722a3cb33cb
leandrogasque <141884738+leandrogasque@users.noreply.github.com>
2023-11-04 00:14:28 -0300

    add: vercel.json and readme.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ProjetoPPIaula2
 Atividade Avaliativa 2 de Programação para Internet módulo I.
+
+Aluno: Leandro Gasque de Melo
+Curso: Analise e Desenvolviemento de Sistemas
+Universidade Do Oeste Paulista - Unoeste
+Faculdade de Informatica de Presidente Prudente - FIPP
+
+
+# Link do Projeto no render: https://proetoppileandrogasque.onrender.com
--- index.js
+++ index.js
@@ -105,7 +105,7 @@ app.get('/', (requisicao, resposta) => {
     resposta.end(` <!doctype html>
     <html lang="pt-br">
     <head>
-      <title>TESTE</title>
+      <title>MENU</title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
